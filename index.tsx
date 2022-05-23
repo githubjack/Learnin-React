8f07b3505b69841e5428f365020369fa96c32a26
githubjack <jack.ui.maynard@gmail.com>
2022-05-23 16:41:29 +0100

    using props and name change template or component

--- index.tsx
+++ index.tsx
@@ -7,63 +7,38 @@ const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 {/**
-const LakeList = [
-  { id: '1', name: 'Echo', trailhead: 'Echo'},
-  { id: '2', name: 'Maud', trailhead: 'Maudster'},
-  { id: '3', name: 'Rose', trailhead: 'Rose Water'},
-];
+Conditionally rendering using props: this could be an input to 
+change a background color through checkbox checked or not equals different template using props and name
 */}
 
-const list = [1, 2, 3, 4, 5]
-
-function App({ items }) {
+function Lake({name}) {
   return (
     <div>
-      
-
-      {
-        /**
-         * 
-         *       
-         * <ul>
-            {lakes.map(lake => (
-              <li>{lake}</li>
-            ))}
-          </ul>
-         */
-      }
-
-      {
-        /*
-      <div>
-        {lakes.map(lake => (
-          <div key={lake.id}>
-            <h2>{lake.name} {lake.id}</h2>
-            <p>Trailhead: <u>{lake.trailhead}</u></p>
-          </div>
-        ))}
-      </div>
-        */ 
-      }
-      {
-        /*
-        Conditional rendering
-        */
-      }
-        <ul>
-          {items.map(item => (
-            <li key={item.toString()}>{item}</li>
-          ))}
-        </ul>
+      <h1>Visit {name}</h1>
     </div>
   );
 }
 
+function SkiResort({name}) {
+  return (
+    <div>
+      <h1>Visit {name}</h1>
+    </div>
+  );
+}
 
+function App(props) {
+ if(props.season === 'summer') {
+   return <Lake name='Jack Lake!'/>
+ } else if (props.season === 'spring') {
+   return <SkiResort name='Jackson Mountain Resort!' />
+ }
+
+}
 root.render(
   <StrictMode>
     <div>
-      <App items={list} />
+      <App season='spring'/>
     </div>
   </StrictMode>
 );
