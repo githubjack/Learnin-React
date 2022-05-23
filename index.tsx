a409291bb455f9535e697074b386bfeea9ebcb23
githubjack <jack.ui.maynard@gmail.com>
2022-05-23 15:33:22 +0100

    conditional rendering

--- index.tsx
+++ index.tsx
@@ -6,48 +6,55 @@ import './style.css';
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
+{/**
+const LakeList = [
+  { id: '1', name: 'Echo', trailhead: 'Echo'},
+  { id: '2', name: 'Maud', trailhead: 'Maudster'},
+  { id: '3', name: 'Rose', trailhead: 'Rose Water'},
+];
+*/}
 
-// example of JSX ( create a functional component return something and call it by tag name )
-/*
-function Hello({Library, Message, Number}) {
-//  console.log(props);
-  console.log(Object.keys(length));
+const list = [1, 2, 3, 4, 5]
+
+function App({ items }) {
   return (
     <div>
-      <h1>Notifying this is a {Library} component.</h1>
-      <p className="message">This could be a <strong>notify</strong> component main message is <b>{Message}</b></p>
-      <p>Special number is : {Number}</p>
-    </div>
-  )
+      
 
-}
-*/
+      {
+        /**
+         * 
+         *       
+         * <ul>
+            {lakes.map(lake => (
+              <li>{lake}</li>
+            ))}
+          </ul>
+         */
+      }
 
-// example of acessing object properties in react
-/*
-let city = {
-  name: 'Madrid',
-  country: 'Spain',
-  region: 'central spain',
-  hotels: 4050,
-  clubs: 'Real and Athletico',
-  food: 'spanish souse'
-
-};
-*/
-// Component reuse passing different props values
-function Lake({name}) {
-  return (
-    <h1>{name}</h1>
-  );
-}
-function App() {
-  return (
-    <div>
-      <h2>App</h2>
-      <Lake name="Lake tadhoe"/>
-      <Lake name="Lake mae and fog"/>
-      <Lake name="Lake tea at noon"/>
+      {
+        /*
+      <div>
+        {lakes.map(lake => (
+          <div key={lake.id}>
+            <h2>{lake.name} {lake.id}</h2>
+            <p>Trailhead: <u>{lake.trailhead}</u></p>
+          </div>
+        ))}
+      </div>
+        */ 
+      }
+      {
+        /*
+        Conditional rendering
+        */
+      }
+        <ul>
+          {items.map(item => (
+            <li key={item.toString()}>{item}</li>
+          ))}
+        </ul>
     </div>
   );
 }
@@ -56,20 +63,7 @@ function App() {
 root.render(
   <StrictMode>
     <div>
-      {/** 
-       * 
-       * 
-       *
-       * <h1>Hello from {city.name} is in {city.country}</h1>
-      <p>Enjoy your time in {city.region} where you have the choice of <strong>{city.hotels}</strong> to choose from.</p>
-      <p>If you are a <strong>Footie fan</strong> you have a choice {city.clubs} to check out, or get down to district <b>4</b> and enjoy some <strong>{city.food}</strong></p>
-      */}
-{
-  /**
-   *       <Hello Library="React" Message="Have fun" Number={3} />
-   */
-}
-    <App />
+      <App items={list} />
     </div>
   </StrictMode>
 );
