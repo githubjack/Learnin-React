efa4b8783df85606ae4a75c3b89ce21e114a5e44
githubjack <jack.ui.maynard@gmail.com>
2022-06-09 15:42:03 +0100

    Using useReducer

--- index.tsx
+++ index.tsx
@@ -7,34 +7,27 @@ const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 {/**
-  fetching data with useEffect as JSON Stringified
+Using useReducer
 */}
-function GitHubUser({login}) {
-  const [data, setData] = React.useState(null);
-  React.useEffect(() => {
-    fetch(`https://api.github.com/users/${login}`)
-    .then(res => res.json())
-    .then(setData)
-    .catch(console.error);
-  }, []);
-  if (data) {
-    return (
-      <div>
-        <h1>{data.login}</h1>
-        <div className='card'>
-          <img src={data.avatar_url} width='120' />
-        </div>
-      </div>
-    )
-  }
-  return null;
-}
- function App() {
-    return <GitHubUser login='githubjack' />
+
+ function Checkbox() {
+   const [checked, toggle] = React.useReducer(
+    checked => !checked,
+    false
+   );
+
+  return (
+    <React.Fragment>
+      <input type='checkbox'
+             value={checked}
+             onChange={toggle} />
+      {checked ? 'checked' : 'not checked'}
+    </React.Fragment>
+  )
  }
 
 root.render(
   <StrictMode>
-    <App />
+    <Checkbox />
   </StrictMode>
 );
