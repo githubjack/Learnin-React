0a6a1bc94e0eb0d4356a2b029705144fb43e76fb
githubjack <jack.ui.maynard@gmail.com>
2022-06-09 11:02:07 +0100

    Multiple variables with useState

--- index.tsx
+++ index.tsx
@@ -12,10 +12,20 @@ const root = createRoot(rootElement);
 
 
 function App(props) {
+  const [year, setYear] = React.useState(2050);
+  const [manager, setManager] = React.useState('Alex');
   const [status, setStatus] = React.useState('Open');
   return (
     <div>
-      <h1>Status: {status}</h1>
+      <div>
+      <h1>{year}</h1>
+      <button onClick={() => setYear(year +1)}>Set Year</button>
+      </div>
+      <h2>Status: {status}</h2>
+      <div>
+        <h3>Manager on Duty: {manager}</h3>
+        <button onClick={() => setManager('Rachel')}>New Manager</button>
+      </div>
       <div>
         <button onClick={() => setStatus('Open')}>Open</button>
         <button onClick={() => setStatus('Back in 5')}>Break</button>
